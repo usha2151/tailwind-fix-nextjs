e7391afea8759a1f4a6d66c56f70a30d1ca7a1fb
usha2151 <ushasing215105@gmail.com>
2025-04-11 06:33:10 -0400

    updates done

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-accent lg:bg-muted sm:bg-warning md:bg-success xl:bg-secondary 2xl:bg-primary p-24 text-secondary"> 
+      <p>hello</p>
+    </div>
+  );
+}
